117b4ec1d1c94726fc734ebb3bc93a7b4e301634
tmill313 <tmill313@gmail.com>
2021-07-26 20:30:59 -0600

    headers

--- server.js
+++ server.js
@@ -267,7 +267,7 @@ cron.schedule('32 20 * * Sunday', () => {
 
 //   twiml.message('The Robots are coming! Head for the hills!');
 
-  res.writeHead(200);
+  res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
      }
 });
